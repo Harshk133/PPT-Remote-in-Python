292c36b9b8121d9bd1433ab0b640440bf1d1c7af
Harsh Moreshwar Kale <h@rshmkale.2004@gmail.com>
2024-03-22 22:52:47 +0530

    #9

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def start():
         return f'Error changing slide: {str(e)}'
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
